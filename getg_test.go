3d071dc7fb52dbd71620905cf658c1dd320ce854
chenzhuoyu <chenzhuoyu@bytedance.com>
2022-04-29 17:50:09 +0800

    chore: add benchmarks for G()

--- getg_test.go
+++ getg_test.go
@@ -7,3 +7,17 @@ import (
 func TestGetG(t *testing.T) {
     println(G().String())
 }
+
+func BenchmarkGetG_Serial(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        G()
+    }
+}
+
+func BenchmarkGetG_Parallel(b *testing.B) {
+    b.RunParallel(func(pb *testing.PB) {
+        for pb.Next() {
+            G()
+        }
+    })
+}
