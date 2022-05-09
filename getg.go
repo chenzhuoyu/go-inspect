340c445a52ea5e3635bbfdbfbe580f7a6942176c
chenzhuoyu <chenzhuoyu@bytedance.com>
2022-05-09 13:17:53 +0800

    feat: get the labels of a goroutine

--- getg.go
+++ getg.go
@@ -5,9 +5,9 @@ import (
     `unsafe`
 )
 
-
 var (
-    offset = uintptr(0)
+    g_goid   = gfield("goid")
+    g_labels = gfield("labels")
 )
 
 // Goroutine represents a goroutine object.
@@ -15,9 +15,9 @@ type Goroutine struct {
     _ uintptr
 }
 
-// ID returns the goroutine ID.
+// ID returns the Goroutine ID.
 func (self *Goroutine) ID() int {
-    return int(*(*int64)(unsafe.Pointer(uintptr(unsafe.Pointer(self)) + offset)))
+    return int(*(*int64)(unsafe.Pointer(uintptr(unsafe.Pointer(self)) + g_goid)))
 }
 
 // String implements fmt.Stringer.
@@ -25,12 +25,17 @@ func (self *Goroutine) String() string {
     return fmt.Sprintf("<goroutine %d>", self.ID())
 }
 
-func init() {
+// Labels returns the current Goroutine labels.
+func (self *Goroutine) Labels() map[string]string {
+    return **(**map[string]string)(unsafe.Pointer(uintptr(unsafe.Pointer(self)) + g_labels))
+}
+
+func gfield(name string) uintptr {
     if t := FindType("runtime.g"); t == nil {
         panic("cannot find type 'runtime.g'")
-    } else if fp, ok := t.FieldByName("goid"); !ok {
-        panic("cannot get offset of 'goid' from 'runtime.g'")
+    } else if fp, ok := t.FieldByName(name); !ok {
+        panic("cannot get offset of '" + name + "' from 'runtime.g'")
     } else {
-        offset = fp.Offset
+        return fp.Offset
     }
 }
--- getg_test.go
+++ getg_test.go
@@ -1,11 +1,20 @@
 package inspect
 
 import (
+    `context`
+    `fmt`
+    `runtime/pprof`
     `testing`
 )
 
 func TestGetG(t *testing.T) {
     println(G().String())
+}
+
+func TestGLabels(t *testing.T) {
+    pprof.Do(context.Background(), pprof.Labels("key1", "value1", "key2", "value2"), func(_ context.Context) {
+        println(fmt.Sprintf("%#v", G().Labels()))
+    })
 }
 
 func BenchmarkGetG_Serial(b *testing.B) {
